6b4a975e86955dad29f79e4ddf67e325fb1c2b10
Sébastien_P <108793240+SkollLockheart@users.noreply.github.com>
2022-07-26 16:51:22 +0200

    Mise à jour Exo ALGO

--- 4_Mes_Supports_Cours/ALGO/Tableau/Exo/tableaux2.js
+++ 4_Mes_Supports_Cours/ALGO/Tableau/Exo/tableaux2.js
@@ -90,25 +90,96 @@ console.log(existe(tabTest,7));
 console.log(existe(tabTest,208));
 // [Bonus] Suite de l'exo : on **sait** que le tableau reçu est trié (on ne le vérifie pas). Comment exploiter cette information pour améliorer la recherche d'un élément ?
 
+/*let tabprix = Array.from({length: 31}, () => Math.floor(Math.random() * 31));
+tabprix.sort((a,b)=>a-b);
+console.log(tabprix);
 function justePrix(tab,num){
-    let bot = tab[0];
+    let bot = 0;
     let top = tab.length-1;
-    let prix;
+
     for(let i=0;i<tab.length;i++){
-        if(num<(prix=tab[top]/2)){
-            top = prix;
-        } else if (num>(prix=tab[top/2])){
-            bot = prix;
+        let prix = tab[((bot-top)/2)+bot];
+        if(num<prix){
+            top = top/2;
+        } else if (num>prix){
+            bot = top/2;
+        } else if(num=prix){
+            console.log(num+" existe!");
         } else {
-            
-        }
+            console.log(num+" n'existe pas!");
+        }       
     }
 }
+
+console.log(("Résultat Exo 8"));
+justePrix(tabprix,14);*/
+
+let tab7 = []
+for(let i=1;i<7778;i++){
+    tab7.push(i);
+}
+
+console.log(("Résultat Exo 9"));
+console.log(tab7);
+
+function justePrix(tab, num){
+    let begin = 0;
+    let finish = (tab.length-1);
+    
+    while (begin <= finish){
+        console.log(begin, finish);
+        let middle = Math.round((begin + finish)/2);
+        console.log('je regarde ' + middle);
+        if (num == tab[middle]){
+            return true;
+        } else if (num < tab[middle]){
+            finish = middle -1 ;
+        } else{
+            begin = middle +1;
+        }        
+    }
+    return false;
+}
+console.log(("Résultat Exo 8"));
+justePrix(tabprix,14);*/
+
 // Créer un tableau qui contient [1,2,3,...,7777].
+let tab7 = []
+for(let i=1;i<7778;i++){
+    tab7.push(i);
+}
+
+console.log(("Résultat Exo 9"));
+console.log(tab7);
 
 // Créer un tableau qui contient [10,20,30,...,77770].
+let tab70 = []
+for(let i=1;i<7778;i++){
+    tab70.push(i*10);
+}
+
+console.log(("Résultat Exo 10"));
+console.log(tab70);
 
 // En se servant du tableau précédent, créer un tableau qui contient [5,10,15,...,38885].
+let tab5 = []
+for(let i=1;i<38886;i++){
+    tab5.push(i*5);
+}
+
+console.log(("Résultat Exo 11"));
+console.log(tab5);
 
 // Écrire une fonction qui prend un tableau en entrée et qui en supprime les dernières valeurs, tant qu'elles sont inférieures à 50.
-// console.log(leNomDeVotreFonction([1,45,88,54,23,-100,12]); // doit afficher [1,45,88,54]
+function leNomDeVotreFonction(tab){
+    for(let i=0;i<tab.length;i++){
+        if (tab[tab.length-1]<50){
+            tab.pop();
+        } else {
+            return tab
+        }
+    }    
+}
+
+console.log(("Résultat Exo 12"));
+console.log(leNomDeVotreFonction([1,45,88,54,23,-100,12])); // doit afficher [1,45,88,54]
