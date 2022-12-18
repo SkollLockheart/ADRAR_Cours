7a5da44021e65aa20edbc750822a665edd7ea5f7
sporcu <108793240+SkollLockheart@users.noreply.github.com>
2022-12-18 16:44:09 +0100

    Maj projet

---
+++ 3_Mon_Projet_Fil_Rouge/6_Front-end/Front classic/Accueil/app.js
@@ -0,0 +1,9 @@
+const togglerLeft = document.querySelector('#burger_left');
+const asideLeft = document.querySelector('#aside_left');
+
+const toggleAside = () => {
+    togglerLeft.classList.toggle('open')
+    asideLeft.classList.toggle('open')
+}
+
+togglerLeft.addEventListener('click', toggleAside)
