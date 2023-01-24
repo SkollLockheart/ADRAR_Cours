36a6a58a05f0586266b97b1f4d3321e40d3e2dae
unknown <sporcu.dev@gmail.com>
2023-01-24 16:59:14 +0100

    Cours vusJS

---
+++ 4_Mes_Supports_Cours/VUE_JS/Cours/cours-vue-cli-14-component-adv-setup/src/components/BadgeList.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <h2>Badges dispo</h2>
+        <ul>
+            <li>
+                <base-badge type="nul" caption="NUL"></base-badge>
+            </li>
+            <li>
+                <base-badge type="premium" caption="PREMIUM"></base-badge>
+            </li>
+            <li>
+                <base-badge type="toto" caption="toto"></base-badge>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    components: {
+
+    },
+}
+</script>
+
+<style>
+ul {
+list-style: none;
+margin: 0;
+padding: 0;
+display: flex;
+flex-direction: row;
+}
+
+li {
+margin-right: 1rem;
+}
+</style>
